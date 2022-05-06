498c61adc24aab01af2d9d599334989d6ea87867
ThorloX <thorlox94@gmail.com>
2022-05-06 11:46:47 +0200

    Add project files.

---
+++ double params/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DoubleParams
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            // Params keyword = a method parameter that takes a variable number of arguments.
+            //                  the parameter type must be a single - dimensional array.
+
+            double total = CheckOut(3.99, 5.75, 15);
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+
+            static double CheckOut(params double[] prices)
+            {
+                double total = 0;
+                foreach (double price in prices)
+                {
+                    total += price;
+                }
+                return total;
+            }
+
+
+
+        }
+    }
+}
